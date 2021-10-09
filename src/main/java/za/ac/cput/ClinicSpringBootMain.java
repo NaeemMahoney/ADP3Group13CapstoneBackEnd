1367819f5058fd5ee89945bae108b16ad8359856
Na'eem Mahoney <mahoneynaeem@gmail.com>
2021-10-09 17:33:18 +0200

    Added Main

---
+++ src/main/java/za/ac/cput/ClinicSpringBootMain.java
@@ -0,0 +1,15 @@
+package za.ac.cput;
+
+//Group13
+//Capstone - Back-End
+//Clinic Main
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicSpringBootMain.class, args);
+    }
+}
