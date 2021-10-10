5c8d4467b89cdb6b7e83eec94a05e702e556377f
Siphosethu Manisi <219039380@mycput.ac.za>
2021-10-10 16:55:08 +0200

    Revert "Created all classes to do with patient"
    This reverts commit 636fe0755786bfab9e3ccfdf375dafbcaafb69ad.

--- src/main/java/za/ac/cput/repository/person/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository.person;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.ac.cput.entity.person.Patient;
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, String> {
-
-}
